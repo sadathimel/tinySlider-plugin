613c72de7bedd8716e07f2fea682e6c664ad735b
Mahadi001 <sadat.himel@gmail.com>
2021-06-21 13:22:29 +0600

    update

--- tinyslider.php
+++ tinyslider.php
@@ -1,109 +1,76 @@
-
 <?php
-/**
- * Template Name: TinySlider Page
+/*
+Plugin Name: TinySlider
+Plugin URI: http://example.com/
+Description: This plugin for slider
+Version: 1.0
+Author: sadathimel
+Author URI: http://github.com/sadathossen
+License: GPLv2 or later
+Text Domain: tinyslider
+Domain Path: /languages
  */
- the_post();
- get_header();
-?>
 
+function tinys_load_textdomain() {
+	load_plugin_textdomain( 'tinyslider', false, dirname( __FILE__ ) . "/languages" );
+}
 
-    <!-- s-content
-    ================================================== -->
-    <section class="s-content s-content--narrow s-content--no-padding-bottom">
+add_action( 'plugins_loaded', 'tinys_load_textdomain' );
 
-        <article class="row format-standard">
+function tinys_init(){
+	add_image_size('tiny-slider',800,600,true);
+}
+add_action('init','tinys_init');
 
-            <div class="s-content__header col-full">
-                <h1 class="s-content__header-title">
-                    <?php the_title();?>
-                </h1>
-                <ul class="s-content__header-meta">
-                    <li class="date"><?php the_date();?></li>
-                    <li class="cat">
-                        In
-                        <?php echo get_the_category_list( " " ); ?>
-                    </li>
-                </ul>
-            </div> <!-- end s-content__header -->
+function tinys_assets(){
+	wp_enqueue_style('tinyslider-css','//cdnjs.cloudflare.com/ajax/libs/tiny-slider/2.8.4/tiny-slider.css',null,'1.0');
+	wp_enqueue_script('tinyslider-js','//cdnjs.cloudflare.com/ajax/libs/tiny-slider/2.8.4/min/tiny-slider.js',null,'1.0',true);
 
-            <div class="s-content__media col-full">
-                <div class="s-content__post-thumb">
-                    <?php the_post_thumbnail( "large" );?>
-                </div>
-            </div> <!-- end s-content__media -->
+	wp_enqueue_script('tinyslider-main-js',plugin_dir_url(__FILE__)."/assets/js/main.js",array('jquery'),'1.0',true);
+}
+add_action('wp_enqueue_scripts','tinys_assets');
 
-            <div class="col-full s-content__main">
+function tinys_shortcode_tslider( $arguments, $content ) {
+	$defaults   = array(
+		'width'  => 800,
+		'height' => 600,
+		'id'     => ''
+	);
+	$attributes = shortcode_atts( $defaults, $arguments );
+	$content    = do_shortcode( $content );
 
-                <?php the_content();?>
+	$shortcode_output = <<<EOD
+<div id="{$attributes['id']}" style="width:{$attributes['width']};height:{$attributes['height']}">
+	<div class="slider">
+	{$content}
+	</div>
+</div>
+EOD;
 
-                <p class="s-content__tags">
-                    <span>Post Tags</span>
+	return $shortcode_output;
 
-                    <span class="s-content__tag-list">
-                        <?php echo get_the_tag_list(); ?>
-                    </span>
-                </p> <!-- end s-content__tags -->
+}
 
-                <div class="s-content__author">
-                    <?php echo get_avatar( get_the_author_meta( "ID" ) ); ?>
+add_shortcode( 'tslider', 'tinys_shortcode_tslider' );
 
-                    <div class="s-content__author-about">
-                        <h4 class="s-content__author-name">
-                            <a href="<?php echo esc_url( get_author_posts_url( get_the_author_meta( "ID" ) ) ); ?>">
-                                <?php the_author();?>
-                            </a>
-                        </h4>
+function tinys_shortcode_tslide( $arguments ) {
+	$defaults   = array(
+		'caption'  => '',
+		'id'     => '',
+		'size'=>'tiny-slider'
+	);
+	$attributes = shortcode_atts( $defaults, $arguments );
 
-                        <p>
-                            <?php the_author_meta( "description" );?>
-                        </p>
+	$image_src = wp_get_attachment_image_src($attributes['id'],$attributes['size']);
 
-                        
-                    </div>
-                </div>
+	$shorcode_output = <<<EOD
+<div class='slide'>
+<p><img src="{$image_src[0]}" alt="{$attributes['caption']}"></p>
+<p>{$attributes['caption']}</p>
+</div>
+EOD;
 
-                <div class="s-content__pagenav">
-                    <div class="s-content__nav">
-                        <div class="s-content__prev">
-                            <?php
-                             $philosophy_prv_post = get_previous_post();
-                             if ( $philosophy_prv_post ):
-                            ?>
-                            <a href="<?php echo get_the_permalink( $philosophy_prv_post ); ?>" rel="prev">
-                                <span><?php _e( "Previous Post", "philosophy" );?></span>
-                                <?php echo get_the_title( $philosophy_prv_post ); ?>
-                            </a>
-                            <?php endif;?>
-                        </div>
-                        <div class="s-content__next">
-                            <?php
-                             $philosophy_next_post = get_next_post();
-                             if ( $philosophy_next_post ):
-                            ?>
-                            <a href="<?php echo get_the_permalink( $philosophy_next_post ); ?>" rel="next">
-                                <span><?php _e( "Next Post", "philosophy" );?></span>
-                                <?php echo get_the_title( $philosophy_next_post ); ?>
-                            </a>
-                            <?php endif;?>
-                        </div>
-                    </div>
-                </div> <!-- end s-content__pagenav -->
+	return $shorcode_output;
+}
 
-            </div> <!-- end s-content__main -->
-
-        </article>
-
-
-        <!-- comments
-        ================================================== -->
-        <?php
-         if ( !post_password_required() ) {
-          comments_template();
-         }
-        ?>
-
-    </section> <!-- s-content -->
-
-
-   <?php get_footer();?>
+add_shortcode( 'tslide', 'tinys_shortcode_tslide' );
